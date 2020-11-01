fbb0e021bb40a6e0d09a59658e628b247eaa0cce
RaduRS <rsrusu90@gmail.com>
2020-11-01 15:01:58 +0000

    Updates

---
+++ precache-manifest.fa5cc323b89e51f8cbe7872513af5758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b0618e82864fbdf712f152db71860a",
+    "url": "/guitar-sound-sourcecode/index.html"
+  },
+  {
+    "revision": "57c07a02189275edcc5b",
+    "url": "/guitar-sound-sourcecode/static/css/main.4121fe43.chunk.css"
+  },
+  {
+    "revision": "749a93c4ec5cf44c1f02",
+    "url": "/guitar-sound-sourcecode/static/js/2.5098435f.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/guitar-sound-sourcecode/static/js/2.5098435f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c07a02189275edcc5b",
+    "url": "/guitar-sound-sourcecode/static/js/main.23299f15.chunk.js"
+  },
+  {
+    "revision": "369cd077771b8563e29a",
+    "url": "/guitar-sound-sourcecode/static/js/runtime-main.f14a9e1a.js"
+  },
+  {
+    "revision": "093bf7d6278a95cd13178f9af51aa708",
+    "url": "/guitar-sound-sourcecode/static/media/logo.svg.093bf7d6.svg"
+  }
+]);
